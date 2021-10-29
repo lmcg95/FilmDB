dba6634c3e5f00013e0492c794aff93cd88e7b58
Lauren <lmcgonnagle@softwareinstitute.com>
2021-10-29 15:51:26 +0100

    some stuff

--- src/main/java/com/film/FilmDatabase/Actor.java
+++ src/main/java/com/film/FilmDatabase/Actor.java
@@ -1,10 +1,31 @@
 package com.film.FilmDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Actor {
 
     ///////////////////////////ATTRIBUTES////////////////////////////////////
 
-    //////////////////////////CONSTRUCTORS//////////////////////////////////
+    private String name;
+    private int actorID;
+    private List<Film> films = new ArrayList();
 
+
+    //////////////////////////CONSTRUCTORS//////////////////////////////////
+public Actor(String name, int actorID){
+    this.name = name;
+    this.actorID = actorID;
+}
     ////////////////////////////METHODS////////////////////////////////////
+
+    public String getName(){
+    return name;
+    }
+
+    public int getActorID(){
+    return actorID;
+    }
+
+
 }
