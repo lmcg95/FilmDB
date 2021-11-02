48bb0204207e8ef25ca82338eb445378fee53b8c
Lauren <lmcgonnagle@softwareinstitute.com>
2021-11-02 10:26:55 +0000

    UNIT TESTS

--- src/test/java/FilmTest.java
+++ src/test/java/FilmTest.java
@@ -1,11 +1,30 @@
 
 
 
+import com.film.FilmDatabase.Film;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FilmTest {
+    @Test
+    public void testTitle(){
+        Film film = new Film("The Breakfast Club", 1980);
+        assertEquals("The film was not the same Title","The Breakfast Club",film.getTitle());
+    }
+
+    @Test
+    public void testYear(){
+        Film film = new Film("The Breakfast Club",1980);
+        assertEquals("The film was not released the same year", 1980, film.getRelease_year());
+    }
+
+    @Test
+    public void testID(){
+        Film film = new Film("The Breakfast Club",1980);
+        film.setFilm_id(12);
+        assertEquals("The film was not the same id", 12, film.getFilm_id());
+    }
 
 
 }
---
+++ src/test/java/com/film/FilmDatabase/ActorTest.java
@@ -0,0 +1,27 @@
+package com.film.FilmDatabase;
+
+import com.film.FilmDatabase.Actor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ActorTest{
+    @Test
+    public void testFirstName(){
+        Actor actor = new Actor("Penelope", "Guiness");
+        assertEquals("The actor did not have the same first name", "Penelope", actor.getFirst_name());
+    }
+
+    @Test
+    public void testLastName(){
+        Actor actor = new Actor("Jennifer", "Lawrence");
+        assertEquals("The actor did not have the same last name", "Lawrence", actor.getLast_name());
+    }
+
+    @Test
+    public void testActorID(){
+        Actor actor = new Actor("Joe","Pesci");
+        actor.setActor_id(12);
+        assertEquals("The actor did not have the same ID", 12, actor.getActor_id());
+    }
+}
