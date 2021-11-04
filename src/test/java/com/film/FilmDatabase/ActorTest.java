8ca8a493b4cfec81212d6e048c32fb5481f3f7f2
Lauren <lmcgonnagle@softwareinstitute.com>
2021-11-04 09:18:36 +0000

    junit 5

--- src/test/java/com/film/FilmDatabase/ActorTest.java
+++ src/test/java/com/film/FilmDatabase/ActorTest.java
@@ -1,60 +1,60 @@
 package com.film.FilmDatabase;
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ActorTest{
     @Test
     public void testFirstName(){
         Actor actor = new Actor("Penelope", "Guiness");
-        assertEquals("The actor did not have the same first name", "Penelope", actor.getFirst_name());
+        assertEquals("Penelope", actor.getFirst_name(),"The actor did not have the same first name");
     }
 
     @Test
     public void testLastName(){
         Actor actor = new Actor("Jennifer", "Lawrence");
-        assertEquals("The actor did not have the same last name", "Lawrence", actor.getLast_name());
+        assertEquals( "Lawrence", actor.getLast_name(),"The actor did not have the same last name");
     }
 
     @Test
     public void testActorID(){
         Actor actor = new Actor("Joe","Pesci");
         actor.setActor_id(12);
-        assertEquals("The actor did not have the same ID", 12, actor.getActor_id());
+        assertEquals( 12, actor.getActor_id(),"The actor did not have the same ID");
     }
 
     @Test
     public void testSetFirst(){
         Actor actor = new Actor();
         actor.setFirst_name("Jim");
-        assertEquals("The actor did not have the same first name", "Jim", actor.getFirst_name());
+        assertEquals( "Jim", actor.getFirst_name(),"The actor did not have the same first name");
     }
 
     @Test
     public void testSetLast(){
         Actor actor = new Actor();
         actor.setLast_name("McGonnagle");
-        assertEquals("The actor did not have the same last name", "McGonnagle", actor.getLast_name());
+        assertEquals( "McGonnagle", actor.getLast_name(),"The actor did not have the same last name");
     }
 
     @Test
     public void testFirstName2(){
         Actor actor = new Actor("Lauren", "McGonnagle", 1);
-        assertEquals("The actor did not have the same first name", "Lauren", actor.getFirst_name());
+        assertEquals( "Lauren", actor.getFirst_name(),"The actor did not have the same first name");
     }
 
     @Test
     public void testLastName2(){
         Actor actor = new Actor("Lauren", "McGonnagle", 1);
-        assertEquals("The actor did not have the same last name", "McGonnagle", actor.getLast_name());
+        assertEquals( "McGonnagle", actor.getLast_name(),"The actor did not have the same last name");
     }
 
     @Test
     public void testID2(){
         Actor actor = new Actor("Lauren", "McGonnagle", 1);
-        assertEquals("The actor did not have the same ID", 1, actor.getActor_id());
+        assertEquals( 1, actor.getActor_id(),"The actor did not have the same ID");
     }
 
 }
