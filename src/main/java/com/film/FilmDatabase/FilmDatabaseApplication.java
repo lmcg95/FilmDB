08c58db6490b60df175be02833e3fbcf36e71a4c
Lauren <lmcgonnagle@softwareinstitute.com>
2021-11-24 11:11:39 +0000

    jenkins

--- src/main/java/com/film/FilmDatabase/FilmDatabaseApplication.java
+++ src/main/java/com/film/FilmDatabase/FilmDatabaseApplication.java
@@ -3,6 +3,9 @@ package com.film.FilmDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
@@ -36,28 +39,37 @@ public class FilmDatabaseApplication {
 		return actorRepository.findAll();
 	}
 
-	@GetMapping("/actorsByID{actor_id}")
+	@GetMapping("/actorsByID/{actor_id}")
 	public @ResponseBody
 	Optional<Actor> getAllUsers3(@PathVariable ("actor_id") int actor_id){
 		return actorRepository.findById(actor_id);
 	}
 
-	@GetMapping("/filmsByID{film_id")
+	@GetMapping("/filmsByID/{film_id}")
 	public @ResponseBody
 	Optional<Film> getAllUsers4(@PathVariable ("film_id") int film_id){
 		return filmRepository.findById(film_id);
 	}
 
 
-	@PostMapping("/addFilm")
-	public @ResponseBody String addAFilm (@RequestParam String title
-			, @RequestParam int release_year, @RequestParam String description, @RequestParam int length, @RequestParam String rating) {
+//	@PostMapping("/addFilm")
+//	@CrossOrigin(origins = "http://localhost:3000")
+//	public @ResponseBody String addAFilm (@RequestParam String title
+//			, @RequestParam int release_year, @RequestParam String description, @RequestParam int length, @RequestParam String rating) {
+//
+//
+//		Film savedFilm = new Film(title, release_year, description, length, rating);
+//		filmRepository.save(savedFilm);
+//		return "Saved";
+//
+//	}
 
-
-		Film savedFilm = new Film(title, release_year, description, length, rating);
+	@PostMapping(path="/addFilm", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+	@CrossOrigin(origins = "http://localhost:3000")
+	public ResponseEntity<Film> addAFilm(@RequestBody Film newFilm){
+		Film savedFilm = new Film(newFilm.getTitle(), newFilm.getRelease_year(), newFilm.getDescription(), newFilm.getLength(), newFilm.getRating());
 		filmRepository.save(savedFilm);
-		return "Saved";
-
+		return new ResponseEntity<Film>(savedFilm, HttpStatus.OK);
 	}
 
 	@PostMapping("/addActor")
@@ -77,6 +89,7 @@ public class FilmDatabaseApplication {
 	}
 
 	@DeleteMapping("/deleteFilm{film_id}")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public String deleteFilm(@PathVariable ("film_id") int film_id){
 		filmRepository.deleteById(film_id);
 		return "Film deleted";
