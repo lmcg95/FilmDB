2035e6b2d15f922d285b739a795262e06e5412d3
Lauren <lmcgonnagle@softwareinstitute.com>
2021-11-24 14:31:07 +0000

    context

--- src/test/java/com/film/FilmDatabase/FilmDatabaseApplicationTests.java
+++ src/test/java/com/film/FilmDatabase/FilmDatabaseApplicationTests.java
@@ -1,13 +1,13 @@
-package com.film.FilmDatabase;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FilmDatabaseApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.film.FilmDatabase;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FilmDatabaseApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
